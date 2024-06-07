82b729e0b5c18fe8d43cf7551ea3d5f72146f20f
Yumiowari <159826640+yumiowari@users.noreply.github.com>
2024-06-07 11:18:50 -0300

    Removi índices redundantes

--- SQL/index-schema.sql
+++ SQL/index-schema.sql
@@ -1,68 +1,37 @@
 -- CLASSIFICATIONS
 CREATE INDEX classifications_name_index
-ON classifications
-USING btree (name)
+ON classifications USING btree (name)
 
 CREATE INDEX classifications_segmentName_index
-ON classifications
-USING btree (segmentName)
+ON classifications USING btree (segmentName)
 
 -- VENUES
 CREATE INDEX venues_name_index
-ON venues
-USING btree (name)
+ON venues USING btree (name)
 
 CREATE INDEX venues_postalCode_index
-ON venues
-USING btree (postalCode)
+ON venues USING btree (postalCode)
 
 CREATE INDEX venues_zipCode_index
-ON venues
-USING btree (city, state, country, address)
-
-CREATE INDEX venues_city_index
-ON venues
-USING btree (city)
-
-CREATE INDEX venues_state_index
-ON venues
-USING btree (state)
-
-CREATE INDEX venues_address_index
-ON venues
-USING btree (address)
-
-CREATE INDEX venues_city_state_index
-ON venues
-USING btree (city, state)
-
-CREATE INDEX venues_city_address_index
-ON venues
-USING btree (city, address)
+ON venues USING btree (city, state, country, address)
 
 -- ATTRACTIONS
 CREATE INDEX attractions_name_index
-ON attractions
-USING btree (name)
+ON attractions USING btree (name)
 
 -- EVENTS
 CREATE INDEX events_name_index
-ON events
-USING btree (name)
+ON events USING btree (name)
 
 CREATE INDEX events_sale_period_index
-ON events
-USING btree (startDateSale, endDateSale)
+ON events USING btree (startDateSale, endDateSale)
 
 CREATE INDEX events_startDateEvent_index
-ON events
-USING btree (startDateEvent)
+ON events USING btree (startDateEvent)
 
 CREATE INDEX events_price_range_index
-ON events
-USING btree (minPrice, maxPrice)
+ON events USING btree (minPrice, maxPrice)
 
 -- MARKET
 CREATE INDEX market_name_index
-ON market
-USING btree (market)
+ON market USING btree (market)
